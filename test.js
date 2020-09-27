0812751d92cfdbc6c2669f7cc6e3fadba913f4f8
Aga <agasmialek2020@gmail.com>
2020-09-27 15:19:37 +0200

    If and else

--- test.js
+++ test.js
@@ -1,1 +1,33 @@
-  alert ("hello world");
+var youlikemeat = true;
+var myNum = 10;
+
+
+if (youlikemeat) {
+  document.write ("you like meat");
+}
+
+ else {
+       
+   document.write("you hate meat");
+
+ }
+
+
+/*if (myNum == 10) {
+   document.write ("myNum is equal to 10");
+}
+
+  else {
+        
+    document.write("myNu is not equal to 10")
+
+  }
+
+/*if (ilikemeat) /*we cheeck if this is true. if true we will execute below code */ {
+
+   /* document.write("this expresion is true"); */
+
+}      /* between curly brackets scode 
+        block that we will execute if this condition is tru*/
+
+       
