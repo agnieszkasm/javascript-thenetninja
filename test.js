59428a0f372b66d70008e14abf4756b4f2c03d3d
Aga <agasmialek2020@gmail.com>
2020-09-30 22:47:50 +0200

    strings

--- test.js
+++ test.js
@@ -1,22 +1,42 @@
 
- /*SO: user entered apple and 5. you wanted 2 numbers.
- we have to use if statement to check if he put two numbers*/
- var a = "apple";
- var b = 5;
- 
- //double negative
- if (!isNaN(a)) {  //so this is false! not not a number
- 
- /*Above: is this (a) not a number? so checking of a
- is not a number so this will return true*/
- console.log ("meaning of life is " + (a * b));
+/* comparing string*/
 
- 
- }
- /*
- else {
-   
-    console.log("that iant a number");
+ var string1 = "abc";
+ var string2 = "ABC";
+
+ console.log(string1.toLowerCase() === string2.toLowerCase());
+
+ var string1 = "abc";
+ var string2 = "bcd";
+
+ console.log(string1 < string2);
+ /*This is true as... a is before b in the alphabet... so thats seen less than b.. */
+
+
+ var string1 = "abc";
+ var string2 = "Bcd";
+ console.log(string1 < string2);
+ /*This is false.. as... a is before b in the alphabet... so thats seen less than b.. */
+
+
+
+/*PREVIOUS EXAMPLES*/
+/*var myString = 'I am a "fun" string'; /*OK*/
+/*var myString = "I am a "fun"string"; this is not correct*/
+/*var myString = 'I\'m a "fun ninja" string';  */
+
+/*if (myString.indexOf("ninja") === -1) { */
+ /*ok, i am looking for the word ninja. It is not in the 
+ string so it say. OK, I will make it equal to -1
+ so the statement is true, so this code is executed.*/
+
+/*
+        console.log("the word ninja is not in the string");
 }
 
-*/
+else  {
+    console.log("the word ninja starts at position " + myString.indexOf("ninja"));
+}
+
+/*
+console.log (myString.indexOf("string")); */
