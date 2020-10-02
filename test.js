b7a9d88d58bfdb08cb00370a15714cac5c6ae6c5
Aga <agasmialek2020@gmail.com>
2020-10-02 08:46:57 +0200

    THIS keyword_2

--- test.js
+++ test.js
@@ -1,33 +1,21 @@
     var myCar2 = {
         
-        maxSpeed: 50, 
+        maxSpeed: 70, 
         driver: "Net Ninja",
         drive: function(speed, time) {
             console.log(speed * time);
         },
-        test: function() {
-            console.log(this);
+      
+        logDriver: function() {
+            console.log("driver name is " + this.driver);
 
         }
+        /*UP: this again refers to myCar2. 
+        Jakbys napisala zamiast this myCar2 tez by zadzialalo.
+        Ale w przypadku 10 takich opcji to oszzcednisc czasu*/
     };  
 
-
-    var myCar3 = {
-        
-        maxSpeed: 50, 
-        driver: "Shaun",
-        drive: function(speed, time) {
-            console.log(speed * time);
-        },
-        test: function() {
-            console.log(this);
-
-        }
-    };  
-   
-    
-        myCar2.test();
-        myCar3.test();
+        myCar2.logDriver();
         console.log(myCar2.maxSpeed);
         myCar2.drive(50, 3);
     
