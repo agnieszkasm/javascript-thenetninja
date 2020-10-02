9db11ac6b5ed2a81f7f2458fd86b6741f82703af
Aga <agasmialek2020@gmail.com>
2020-10-02 10:07:51 +0200

    31. THE DATE OBJECT

--- test.js
+++ test.js
@@ -1,24 +1,62 @@
-var Car = function(maxSpeed, driver) {
+var myDate = new Date(); /*this will store current date for u*/
 
-        this.maxSpeed = maxSpeed;
-        this.driver = driver;
-        this.drive = function(speed, time) {
-            console.log(speed * time);
-        };
-        this.logDriver = function() {
-            console.log("driver name is " + this.driver);
-        };
+console.log(myDate);
 
 
-  }
+var myPastDate = new Date(1984, 2, 2, 10, 30, 55);
+var myFuturetDate = new Date(2045, 0, 31, 10, 30, 55);
 
-  /*below we have 4 objects and abone one contructor function*/
-  var myCar = new Car(10, "Ninja man");
-  var myCar2 = new Car(60, "A man");
-  var myCar3 = new Car(70, "Aga");
-  var myCar4 = new Car(10000, "DDD");
+console.log(myPastDate);
+console.log(myFuturetDate);
 
-  myCar.drive(30,5);
-  myCar3.logDriver();
-  myCar4.logDriver();
-  myCar4.drive(33,10);
+
+
+var birthday = new Date(1985, 0, 15, 11, 50, 25);
+var birthday2 = new Date(1985, 0, 15, 11, 50, 25);
+
+/*get the month of the date 0 -11. Console will show zero -january*/
+console.log(birthday.getMonth());
+
+/*get full year YYYY*/
+console.log(birthday.getFullYear());
+
+/*get the date of the month (1 - 31)*/
+console.log(birthday.getDate());
+
+/*get the date of the week 0-6 where 0 is Sunday, .. 2 is Tue.*/
+console.log(birthday.getDay());
+
+/*get the hour of the day (0 -23)*/
+console.log(birthday.getHours());
+
+/*get the number of miliseconds since 1st Jan 1970. 
+THIS IS RANDOM DATE BUT WE USE IT A LOT.
+it shows number of miliseconds between those two dates: 
+birthday dat and 1st Jan 1970*/
+console.log(birthday.getTime());
+/*we use it to compare dates*/
+
+
+if(birthday.getTime() == birthday2.getTime()) {
+
+    console.log("birthdays are equal")
+
+} else {
+
+    console.log("birthdays are not equal")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
