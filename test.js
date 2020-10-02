b05b40bcb64e3409e09265213756f28195d8672d
Aga <agasmialek2020@gmail.com>
2020-10-02 08:30:55 +0200

    “THIS” KEYWORD _1

--- test.js
+++ test.js
@@ -1,41 +1,33 @@
-var myArray = new Array ();
-myArray[0] = 8;
-myArray[1] = "hello";
-
-var myCar = new Object();
-myCar.myspeed = 50;
-myCar.driver = "Aga";
-
-console.log(myCar.driver);
-
-myCar.drive = function() {
-    console.log("now driving");};
-
-    myCar.drive(); /*tutaj jest nawias bo robimy call funkcji!
-    wywolujemy ją! wyżeej dopiero ją definiujemy / kiedy ją assign!*/
-
-
-/*SHORTHAND VERISON CURLY BRACKETS*/
-var myCar2 = {
-    maxSpeed: 50, 
-    driver: "the Net ninja",
-    drive: function() {
-        console.log("now driving again");
-    }
-};  
-
-    console.log(myCar2.maxSpeed);
-    myCar2.drive();
-
-
     var myCar2 = {
+        
         maxSpeed: 50, 
-        driver: "the Net Ninja",
+        driver: "Net Ninja",
         drive: function(speed, time) {
             console.log(speed * time);
+        },
+        test: function() {
+            console.log(this);
+
         }
     };  
+
+
+    var myCar3 = {
+        
+        maxSpeed: 50, 
+        driver: "Shaun",
+        drive: function(speed, time) {
+            console.log(speed * time);
+        },
+        test: function() {
+            console.log(this);
+
+        }
+    };  
+   
     
+        myCar2.test();
+        myCar3.test();
         console.log(myCar2.maxSpeed);
         myCar2.drive(50, 3);
     
