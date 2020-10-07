abc98a8146db9eb366496461fc981e3c6fb87c77
Aga <agasmialek2020@gmail.com>
2020-10-07 18:13:26 +0200

    41. WINDOW ONLOAD EVENT setUPEvents

--- test.js
+++ test.js
@@ -1,20 +1,31 @@
-var content = document.getElementById("content");
-var button = document.getElementById("show-more");
-
-button.onclick = function() {
-
-    if(content.className == "open") {
-    //shrink the box
-    content.className = "";
-    button.innerHTML = "Show more";
-    }
-    else {
-        //expand box
-
-        content.className = "open";
-        button.innerHTML = "Show less";
+function setUpEvents () {
 
 
- }
+    var content = document.getElementById("content");
+    var button = document.getElementById("show-more");
+    
+    button.onclick = function() {
+    
+        if(content.className == "open") {
+              //shrink the box
+        content.className = "";
+        button.innerHTML = "Show more";
+        }
+        else {
+            //expand box
+    
+            content.className = "open";
+            button.innerHTML = "Show less";
+            }
+    
+    };
 
 }
+
+    window.onload = function(){
+
+        setUpEvents ();
+
+};
+
+
