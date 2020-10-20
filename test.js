377dccd99e421db00ebea72158f194e4041dd457
Aga <agasmialek2020@gmail.com>
2020-10-21 00:09:07 +0200

    updated set interval and clear interval

--- test.js
+++ test.js
@@ -1,8 +1,31 @@
-var myMessage = document.getElementById ("message");
 
-function showMessage() {
+var colourChanger = document.getElementById("colour-changer");
+var colours = ["red", "blue", "green", "pink"]; /*colors that we want this elemnt to cycle through*/
+var counter = 0;
+/*Above: counter is gonna count in which position we are in the array. 
+when block is red  is gonna be in position ), when counter goes to 1 the 
+we gonna call this blue, when counter goes to 2 we gonna call green*/
 
-    myMessage.className = "show"
+function changeColour (){
+
+
+    if (counter >= colours.length) {
+        counter = 0;
+    }
+
+
+colourChanger.style.background = colours[counter];
+counter++;
+
 }
 
-setTimeout(showMessage, 3000)
+var myTimer = setInterval(changeColour, 3000);
+
+colourChanger.onclick = function () {
+    
+    clearInterval(myTimer);
+    colourChanger.innerHTML= "Timer stopped"
+
+};
+
+
