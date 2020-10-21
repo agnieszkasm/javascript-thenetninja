ccf5b28ef7fe9e032bf704bf03db12813607598b
Aga <agasmialek2020@gmail.com>
2020-10-21 22:50:02 +0200

    forms in javascript

--- test.js
+++ test.js
@@ -1,31 +1,31 @@
 
-var colourChanger = document.getElementById("colour-changer");
-var colours = ["red", "blue", "green", "pink"]; /*colors that we want this elemnt to cycle through*/
-var counter = 0;
-/*Above: counter is gonna count in which position we are in the array. 
-when block is red  is gonna be in position ), when counter goes to 1 the 
-we gonna call this blue, when counter goes to 2 we gonna call green*/
+// var colourChanger = document.getElementById("colour-changer");
+// var colours = ["red", "blue", "green", "pink"]; /*colors that we want this elemnt to cycle through*/
+// var counter = 0;
+// /*Above: counter is gonna count in which position we are in the array. 
+// when block is red  is gonna be in position ), when counter goes to 1 the 
+// we gonna call this blue, when counter goes to 2 we gonna call green*/
 
-function changeColour (){
-
-
-    if (counter >= colours.length) {
-        counter = 0;
-    }
+// function changeColour (){
 
 
-colourChanger.style.background = colours[counter];
-counter++;
+//     if (counter >= colours.length) {
+//         counter = 0;
+//     }
 
-}
 
-var myTimer = setInterval(changeColour, 3000);
+// colourChanger.style.background = colours[counter];
+// counter++;
 
-colourChanger.onclick = function () {
+// }
+
+// var myTimer = setInterval(changeColour, 3000);
+
+// colourChanger.onclick = function () {
     
-    clearInterval(myTimer);
-    colourChanger.innerHTML= "Timer stopped"
+//     clearInterval(myTimer);
+//     colourChanger.innerHTML= "Timer stopped"
 
-};
+// };
 
 
