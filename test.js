03e2085710619e958126c05264e01668fafdeb13
Aga <agasmialek2020@gmail.com>
2020-10-21 23:19:01 +0200

    simple form validationt

--- test.js
+++ test.js
@@ -1,31 +1,15 @@
+var myForm = document.forms.myForm;
+var message = document.getElementById("message");
 
-// var colourChanger = document.getElementById("colour-changer");
-// var colours = ["red", "blue", "green", "pink"]; /*colors that we want this elemnt to cycle through*/
-// var counter = 0;
-// /*Above: counter is gonna count in which position we are in the array. 
-// when block is red  is gonna be in position ), when counter goes to 1 the 
-// we gonna call this blue, when counter goes to 2 we gonna call green*/
+myForm.onsubmit = function () {
 
-// function changeColour (){
+if(myForm.name.value == "") {
+    message.innerHTML = "please eneter a name";
+    }
+    else {
+        message.innerHTML = "";
+        return true;
+    }
 
-
-//     if (counter >= colours.length) {
-//         counter = 0;
-//     }
-
-
-// colourChanger.style.background = colours[counter];
-// counter++;
-
-// }
-
-// var myTimer = setInterval(changeColour, 3000);
-
-// colourChanger.onclick = function () {
-    
-//     clearInterval(myTimer);
-//     colourChanger.innerHTML= "Timer stopped"
-
-// };
-
+};
 
